8f7555065603804aec08d3941bf2c2f2fd9d6757
LucasLacer <lucaslacerdapereira@gmail.com>
2021-08-13 13:24:36 -0300

    including miragejs,fake api request for development only

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,13 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
 import { Container } from "../TransactionsTable/Styles";
 
 export function TrasactionsTable() {
+    useEffect(()=> {
+        api.get('transactions')
+        .then(response => console.log(response.data))
+    },[])
+
     return(
         <Container>
             <table>
